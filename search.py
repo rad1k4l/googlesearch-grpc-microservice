1d803065fb659c1ae5b421b072d4df1773f42c74
rad1k4l <zeynalli169@gmail.com>
2021-04-21 08:48:26 +0400

    Add configuration parsing from command line arguments

--- search.py
+++ search.py
@@ -5,7 +5,7 @@ import logging
 import google_pb2_grpc
 import google_pb2
 import grpc
-
+import argparse
 
 class GoogleSearcherService(google_pb2_grpc.GoogleSearcherServicer):
 
@@ -18,14 +18,26 @@ class GoogleSearcherService(google_pb2_grpc.GoogleSearcherServicer):
         return response
 
 
+# parse arguments from command line
+# returns object, properties: 
+#   addr - type string, grpc serving address
+def getConfig():
+    parser = argparse.ArgumentParser(description='Google Search grpc service')
+    parser.add_argument('--address', dest='addr', 
+        type=str,
+        help='Service listen address. Default value is localhost:42420', default="localhost:42420")
+    
+    return parser.parse_args()
+
+
 def serve():
-    service_addr = '[::]:42420'
+    config = getConfig()
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     google_pb2_grpc.add_GoogleSearcherServicer_to_server(
         GoogleSearcherService(), server)
-    server.add_insecure_port(service_addr)
+    server.add_insecure_port(config.addr)
     server.start()
-    logging.info(f'gRPC service started at {service_addr}')
+    logging.info(f'gRPC service started at {config.addr}')
     server.wait_for_termination()
 
 
